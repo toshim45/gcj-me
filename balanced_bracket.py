a860aefabc412fca82151edce61f4868eb60cfeb
Artiko Wibowo <artikow@gmail.com>
2022-02-12 12:14:44 +0700

    balance bracket challenge

--- balanced_bracket.py
+++ balanced_bracket.py
@@ -2,15 +2,20 @@ def is_bracket_balanced(input):
 	stack = []
 	brackets = {"(":")","[":"]","{":"}"}
 	for i in input:
-		if i in brackets.keys():
+		if i in brackets.keys(): #match open bracket, push to stack
 			stack.append(i)
 		else:
 			if not stack:
 				return False
-			
+			latest_bracket_open = stack.pop()
+			bracket_closed = brackets[latest_bracket_open]
+			if bracket_closed != i:
+				return False
 
 	if stack:
 		return False
 	return True
 
 print("({}) -> ",is_bracket_balanced("({})"))
+print("[()]{}{[()()]()} -> ",is_bracket_balanced("[()]{}{[()()]()}"))
+print("[(]) -> ",is_bracket_balanced("[(])"))
